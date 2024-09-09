014aa7094437725484038fd1b2a66370834f3784
Animesh Dhara <animeshdhara398@gmail.com>
2024-09-09 17:01:21 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+
+export default function About(props) {
+    const pageStyle = {
+        color:props.mode === 'dark'?'white':'#073062',
+        backgroundColor : props.mode === 'dark'?'#073062':'white'
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <div className="accordion accordion-flush my-3" id="accordionFlushExample" style={pageStyle} >
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" style={pageStyle}>
+                                Accordion Item #1
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div className="accordion-body" style={pageStyle}>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo" style={pageStyle}>
+                                Accordion Item #2
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div className="accordion-body" style={pageStyle}>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree" style={pageStyle}>
+                                Accordion Item #3
+                            </button>
+                        </h2>
+                        <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div className="accordion-body" style={pageStyle}>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        </div>
+                    </div>
+                </div>
+                {/* <button className='btn btn-primary my-3' onClick={changeStyle}>{text}</button> */}
+            </div>
+
+        </>
+    )
+}
